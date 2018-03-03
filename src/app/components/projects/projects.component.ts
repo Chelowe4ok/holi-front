232cc0af7ce8b04fa5676a97aecad164ea89eafc
Chelowe4ok <Chelowe4ok@DESKTOP-J3MNB84>
2018-03-03 16:11:36 +0200

    drag and drop UI

---
+++ src/app/components/projects/projects.component.ts
@@ -0,0 +1,125 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { NgGrid, NgGridItem, NgGridConfig, NgGridItemConfig, NgGridItemEvent } from 'angular2-grid';
+
+interface Project {
+  id: number;
+  config: any;
+}
+
+@Component({
+  selector: 'app-projects',
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.scss']
+})
+export class ProjectsComponent implements OnInit {
+
+  projects: any;
+  public groups: Array<any> = [
+    {
+      name: 'Group A',
+      items: [{ name: 'Item A' }, { name: 'Item B' }, { name: 'Item C' }, { name: 'Item D' }]
+    },
+    {
+      name: 'Group B',
+      items: [{ name: 'Item 1' }, { name: 'Item 2' }, { name: 'Item 3' }, { name: 'Item 4' }]
+    }
+  ];
+  listOne: Array<string> = ['Coffee', 'Orange Juice', 'Red Wine', 'Unhealty drink!', 'Water'];
+  configur: Object = { 'dragHandle': '.handle', 'col': 1, 'row': 1, 'sizex': 1, 'sizey': 1 };
+  private boxes: Array<Project> = [];
+  private rgb: string = '#efefef';
+  private curNum;
+  private gridConfig: NgGridConfig = <NgGridConfig>{
+    'margins': [5],
+    'draggable': true,
+    'resizable': false,
+    'max_cols': 0,
+    'max_rows': 0,
+    'visible_cols': 0,
+    'visible_rows': 0,
+    'min_cols': 1,
+    'min_rows': 1,
+    'col_width': 2,
+    'row_height': 2,
+    'cascade': 'up',
+    'min_width': 50,
+    'min_height': 50,
+    'fix_to_grid': false,
+    'auto_style': true,
+    'auto_resize': false,
+    'maintain_ratio': false,
+    'prefer_new': false,
+    'zoom_on_drag': false,
+    'limit_to_screen': true
+  };
+  private itemPositions: Array<any> = [];
+
+  constructor(private http: HttpClient) {
+    const dashconf = this._generateDefaultDashConfig();
+    for (var i = 0; i < dashconf.length; i++) {
+      const conf = dashconf[i];
+      conf.payload = 1 + i;
+      this.boxes[i] = { id: i + 1, config: conf };
+    }
+    this.curNum = dashconf.length + 1;
+  }
+
+  addBox(): void {
+    const conf: NgGridItemConfig = this._generateDefaultItemConfig();
+    conf.payload = this.curNum++;
+    this.boxes.push({ id: conf.payload, config: conf });
+  }
+
+  removeWidget(index: number): void {
+    if (this.boxes[index]) {
+      this.boxes.splice(index, 1);
+    }
+  }
+
+  updateItem(index: number, event: NgGridItemEvent): void {
+    // Do something here
+  }
+
+  onDrag(index: number, event: NgGridItemEvent): void {
+    // Do something here
+  }
+
+  onResize(index: number, event: NgGridItemEvent): void {
+    // Do something here
+  }
+
+  private _generateDefaultItemConfig(): NgGridItemConfig {
+    return { 'dragHandle': '.handle', 'col': 1, 'row': 1, 'sizex': 1, 'sizey': 1 };
+  }
+
+  private _generateDefaultDashConfig(): NgGridItemConfig[] {
+    return [{ 'dragHandle': '.handle', 'col': 1, 'row': 1, 'sizex': 1, 'sizey': 1 },
+    { 'dragHandle': '.handle', 'col': 1, 'row': 1, 'sizex': 1, 'sizey': 1 },
+    { 'dragHandle': '.handle', 'col': 26, 'row': 1, 'sizex': 1, 'sizey': 1 },
+    { 'dragHandle': '.handle', 'col': 51, 'row': 1, 'sizex': 1, 'sizey': 1 },
+    { 'dragHandle': '.handle', 'col': 51, 'row': 1, 'sizex': 1, 'sizey': 1 },
+    { 'dragHandle': '.handle', 'col': 83, 'row': 1, 'sizex': 1, 'sizey': 1 }];
+  }
+
+  getDate(date) {
+    let dateObj = new Date(date);
+    return `${dateObj.getDay()}/${dateObj.getMonth()}/${dateObj.getFullYear()}`;
+  }
+
+  ngOnInit() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Access-Control-Allow-Origin': '*',
+        'Authorization': 'authkey',
+        'userid': '1'
+      })
+    };
+
+    this.http.get<any>('assets/data/projects.json', httpOptions).subscribe(data => this.projects = data);
+  }
+
+  generateProjectGridConfig(project): NgGridItemConfig {
+    return { 'dragHandle': '.handle', 'col': project.id + 25, 'row': 1, 'sizex': 1, 'sizey': 1 }
+  }
+}
