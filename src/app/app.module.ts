bf32f566de4f7c9c4c6e3ca912fedf91d5dcbbbf
Chelowe4ok <Chelowe4ok@DESKTOP-J3MNB84>
2018-03-03 16:17:29 +0200

    drag and drop UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { NgGridModule } from 'angular2-grid';
+import { DragulaModule } from 'ng2-dragula';
+import {DndModule} from 'ng2-dnd';
+// Components
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    NgGridModule,
+    DragulaModule,
+    DndModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
